bd91692e164440d7ad2382c2a0eeb560c022d8b5
anoushka765 <84584554+anoushka765@users.noreply.github.com>
2021-06-11 18:40:29 +0000

    Add files via upload

--- AI-Music-Web-App(PROJECT)/main.js
+++ AI-Music-Web-App(PROJECT)/main.js
@@ -1,6 +1,8 @@
 song = "";
+scoreLeftWrist = 0;
 leftWristX = 0;
 leftWristY = 0;
+
 rightWristX = 0;
 rightWristY = 0;
 
@@ -19,6 +21,18 @@ function setup()
 function draw()
 {
     image(video, 0, 0, 600, 500);
+
+     fill("#FF0000");
+     stroke("#FF0000");
+     if(scoreLeftWrist > 0.2)
+     {
+     circle(leftWristX, leftWristY,20);
+     InNumberleftWristY = Number(leftWristY);
+     remove_decimals = floor(InNumberleftWristY);
+     volume = remove_decimals/500;
+     document.getElementById("volume").innerHTML = "Volume = " + volume;
+     song.setVolume(volume);
+    }
 }
 
 function preload()
@@ -44,6 +58,8 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        song.leftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
